e9982ff77dc760f5f0ca7918bb4dffe670357511
Ethan Hansen <1ethanhansen@gmail.com>
2019-07-12 15:46:32 -0700

    Progress bar and comments
    Inital simple progress bar
    
    Signed-off-by: Ethan Hansen <1ethanhansen@gmail.com>
    
    added verbose logging of percent done
    
    Signed-off-by: Ethan Hansen <1ethanhansen@gmail.com>
    
    Adding comments
    
    Signed-off-by: Ethan Hansen <1ethanhansen@gmail.com>

--- tests/bash_tests.sh
+++ tests/bash_tests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#test different args to get-image-info
 python get-image-info.py --help
-
 python get-image-info.py user.yaml
-
 python get-image-info.py user.yaml --debug
 
+#gets number of columns in results.csv
+#head -l outputs the first row of the file
+#sed removes everything expect commas
+#wc -c gives the character count (includes \n)
 if [ $(head -1 results.csv | sed 's/[^,]//g' | wc -c) != 11 ]; then
 echo "Wrong number of columns in results";
-exit 1;
+exit 1;	#travis build should fail if it get the wrong number of columns
 fi
