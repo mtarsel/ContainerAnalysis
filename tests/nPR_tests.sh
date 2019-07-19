cfccb4147615f3466c00976853f99b23033fe4e9
EthanHans3n <1ethanhansen@gmail.com>
2019-07-19 16:06:45 -0700

    --test option added, new tests created Fixes #7
    the --test option allows the user to specify which app(s) to run
    the new tests test both the --test option and many individual functions
    two new files in docs/ are examples of input files used for testing
    
    Signed-off-by: Ethan Hansen <1ethanhansen@gmail.com>

--- tests/nPR_tests.sh
+++ tests/nPR_tests.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-#test different args to get-image-info
-python get-image-info.py user.yaml
-python get-image-info.py user.yaml --debug
-python get-image-info.py user.yaml --keep
+#set the whole script to exit if any command fails
+set -e
 
 #gets number of columns in results.csv
 #head -l outputs the first row of the file
 #sed removes everything expect commas
 #wc -c gives the character count (includes \n)
-if [ $(head -1 archives/results-$(date +"%d-%b-%Y").csv | sed 's/[^,]//g' | wc -c) != 11 ]; then
-echo "Wrong number of columns in results";
-exit 1;	#travis build should fail if it get the wrong number of columns
-fi
+check_num_columns () {
+	if [ $(head -1 archives/results-$(date +"%d-%b-%Y").csv | sed 's/[^,]//g' | wc -c) != 11 ]; then
+	echo "Wrong number of columns in results";
+	exit 1;	#travis build should fail if it get the wrong number of columns
+	fi
+}
+
+#gets number of lines in results.csv
+check_num_rows () {
+	if [ $(cat archives/results-$(date +"%d-%b-%Y").csv | xargs -l echo | wc -l) == 1 ]; then
+	echo "Only 1 row in results!";
+	exit 1;
+	fi
+}
+
+#test different args to get-image-info
+python get-image-info.py user.yaml
+check_num_columns
+check_num_rows
+
+python get-image-info.py user.yaml --debug
+check_num_columns
+check_num_rows
+
+python get-image-info.py user.yaml --keep
+check_num_columns
+check_num_rows
+
+python get-image-info.py user.yaml --test ibm-cam
+check_num_columns
+check_num_rows
 
 #tests if container-output.log exists
 if [ ! -f container-output.log ]; then exit 1; fi
--- tests/pr_okay_tests.sh
+++ tests/pr_okay_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#set the whole script to exit if any command fails
+set -e
+
 #All the tests in this file can be run w/o user.yaml
 #and without secure Travis environment variables
 
