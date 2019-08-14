e5363d62bcf3b68c5402fc95cba83f8ce78234bc
1ethanhansen <1ethanhansen@gmail.com>
2019-08-14 11:31:05 -0700

    Fixes #59 - Format and add slack info
    Formats the mismatched w/ dash slack message better and posts to slack if
    there are apps with internal mismatches.
    
    Signed-off-by: Ethan Hansen <1ethanhansen@gmail.com>

--- utils/teardown_utils.py
+++ utils/teardown_utils.py
@@ -10,19 +10,17 @@ from json import load
 def print_external_conflict_apps(app_list, dash_dict, sfile):
 	"""Prints out all of the app names that conflict with dashboard."""
 	conflict_list = []
-	i = 0
 	print("\n==== CONFLICT WITH DASHBOARD ====\n")
 	for app in app_list:
 		if not app.matches_dashboard(dash_dict):
 			conflict_list.append(app.name)
 			print(app.name)
-			i += 1
 	if conflict_list != []:
                 sfile.write("\n==== CONFLICT WITH DASHBOARD ====\n")
                 for i in conflict_list:
-                        sfile.write(i)
-	print("Total mismatched: {}".format(i))
-	return i
+                        sfile.write(i + "\n")
+	print("Total mismatched: {}".format(len(conflict_list)))
+	return len(conflict_list)
 
 
 def print_bad_apps(app_list):
@@ -39,18 +37,22 @@ def print_bad_apps(app_list):
 	return i
 
 
-def print_internal_conflict_apps(app_list):
+def print_internal_conflict_apps(app_list, sfile):
 	""" Prints apps where the app's supported architectures are not
 		the same as all of the sub-images' supported architectures.
 	"""
-	i = 0
+	conflict_list = []
 	print("\n==== APP ARCHS CONFLICT WITH IMAGE ARCHS ====\n")
 	for app in app_list:
 		if not app.archs_match:
+			conflict_list.append(app.name)
 			print(app.name)
-			i +=1
-	print("Total internally conflicting apps: {}".format(i))
-	return i
+	if conflict_list != []:
+                sfile.write("\n==== APP ARCHS CONFLICT WITH IMAGE ARCHS ====\n")
+                for i in conflict_list:
+                        sfile.write(i + "\n")
+	print("Total internally conflicting apps: {}".format(len(conflict_list)))
+	return len(conflict_list)
 
 
 def diff_last_files(sfile):
